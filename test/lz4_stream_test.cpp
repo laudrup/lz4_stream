587268d9b11dd96b1a8521dbae9005050a9c5480
Kasper Laudrup <laudrup@stacktrace.dk>
2019-02-08 01:37:24 +0100

    Add catch2 main executable
    Add catch2 main application to speed up compilation

--- test/lz4_stream_test.cpp
+++ test/lz4_stream_test.cpp
@@ -1,7 +1,7 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
-
 #include <string>
+#include <sstream>
+
+#include "catch.hpp"
 
 #include "lz4_stream.h"
 
